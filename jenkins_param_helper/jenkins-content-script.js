047b11751f961592a2afaf659642932f418d9ffa
GiraKoo <girakoo@163.com>
2024-01-11 23:26:17 +0800

    feat: upload jenkins_param_helper resource.
    Signed-off-by: GiraKoo <girakoo@163.com>

---
+++ jenkins_param_helper/jenkins-content-script.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         	Jenkins编译辅助插件
+// @version      	1.4.0
+// @description  	进行编译的时候，提供额外的筛选按钮。如果功能不好使，请查看match路径是否正确
+// @author       	girakoo@163.com
+// @updateURL    	https://res.girakoo.com/jenkins_param_helper/jenkins-content-script.js
+// @downloadURL  	https://res.girakoo.com/jenkins_param_helper/jenkins-content-script.js
+// @match      		*://*/*job/*/build*
+// @run-at       	document_end
+// ==/UserScript==
+
+// @history			1.4.0 移除特定IP的限制，通过匹配符进行访问
+
+var _headNode = document.getElementsByTagName('head')[0];
+var script = document.createElement("script");  //创建一个script标签
+script.type = "text/javascript";
+script.src = "https://res.girakoo.com/jenkins_param_helper/jenkins-function-script.js";
+_headNode.appendChild(script);
+
+//第一次加载的时候初始化样式文件
+var setting_main_nodes = document.getElementsByClassName("setting-main");
+for (var loopIdx = 0; loopIdx < setting_main_nodes.length; ++loopIdx) {
+	var input_node = document.createElement("input");
+	var select_node = setting_main_nodes[loopIdx].getElementsByTagName("select")[0];
+
+
+	if (select_node != undefined) {
+		select_node.setAttribute("oninput", "showTip(this)");
+
+		var select_value = select_node.options[select_node.selectedIndex].value;
+		var input_node_id = setting_main_nodes[loopIdx].getElementsByTagName("input")[0].value + "_input_select";
+		input_node.setAttribute("id", input_node_id);
+		input_node.setAttribute("oninput", "showTip(this)");
+		input_node.setAttribute("class", "setting-input");
+		input_node.setAttribute("value", select_value);
+		input_node.setAttribute("onfocus", "clickInput(this)");
+		input_node.setAttribute("style", "width:400px;background-color:#F0F8FF");
+		setting_main_nodes[loopIdx].getElementsByTagName("div")[0].insertBefore(input_node, select_node);
+
+		var _cssNode = document.createElement("style");
+		var _cssContent = "#" + input_node_id + "_div div{background-color:#F0F8FF;}";
+		_cssContent += "#" + input_node_id + "_div div:hover{background-color:#BFEFFF;}";
+		_cssNode.innerHTML = _cssContent;
+		_headNode.appendChild(_cssNode);
+	}
+}
---
+++ jenkins_param_helper/jenkins-function-script.js
@@ -0,0 +1,129 @@
+﻿function clickInput(input) {
+	input.select();
+	showTip(input);
+}
+
+function hideTip(input) {
+	var select_node = input.nextSibling;
+	if (select_node != undefined)
+	{
+		input.value = select_node.options[select_node.selectedIndex].value;
+	}
+	
+	var filter_div_node = document.getElementById("extension_filter_div");
+	if (filter_div_node)
+		filter_div_node.parentNode.removeChild(filter_div_node);
+}
+
+/**
+ *   显示提示信息
+ *   inputId:输入input框的id值
+ */
+function showTip(input) {
+	var inputId = input.id;
+	var _inputNode = document.getElementById(inputId),
+		_inputValue = _inputNode.value,
+		_parentNode = _inputNode.parentNode,
+		_divNode = document.createElement("div");
+
+	var totalOptions = _inputNode.nextSibling.getElementsByTagName("option");
+	var totalValues = new Array(totalOptions);
+	for (var loopIdx = 0; loopIdx < totalOptions.length; ++loopIdx) {
+		totalValues[loopIdx] = totalOptions[loopIdx].value.replace(/\ +/g, "").replace(/[\r\n]/g, "");
+	}
+
+	//如果已经存在了divNode 则删除
+	var filter_div_node = document.getElementById("extension_filter_div");
+	if (filter_div_node)
+		filter_div_node.parentNode.removeChild(filter_div_node);
+
+	//如果没有输入值的时候则无需显示提示框
+	//if (_inputValue == '')
+	//	return;
+
+	var config = {
+		
+		divHeight: "270px",
+		divWidth: "400px",
+		divBorder: "1px solid gray",
+		divBackgroundColor: '#ffffff',
+		overflowY: "scroll",
+		inputHeight: 20,
+		top: 20
+	};
+
+	var _offsetPosition = getPosition(_inputNode);
+
+	//显示待选div样式
+	_divNode.id = "extension_filter_div";
+	_divNode.style.backgroundColor = config.divBackgroundColor;
+	_divNode.style.height = config.divHeight;
+	_divNode.style.width = config.divWidth;
+	_divNode.style.overflowY = config.overflowY;
+	_divNode.style.display = "block";
+	_divNode.style.border = config.divBorder;
+	_divNode.style.position = "absolute";
+	if (config.top > 0)
+		_divNode.style.top = config.top;
+	else
+		_divNode.style.top = (_offsetPosition.y + config.inputHeight) + "px";
+	_divNode.style.left = _offsetPosition.x + "px";
+
+	_divNode.innerHTML = "";
+	var _divHtml = "<div class='task' onclick=\"hideTip(" + inputId + ")\">######### 关闭 #########</div>";
+	var _isExist = false; //用于判断是否存在需要提示的信息 如果是否则无需显示提示框
+	for (var i = 0; i < totalValues.length; i++) {
+		var _tempValue = totalValues[i];
+		if (isIncluded(_tempValue, _inputValue)) {
+			_isExist = true;
+			_divHtml += "<div class='task' onclick=\"_inputDivClick(\'" + inputId + "\',\'" + _divNode.id + "\',\'" +
+				_tempValue + "\')\">" + _tempValue + "</div>";
+		}
+	}
+	_divNode.innerHTML = _divHtml;
+	if (_isExist)
+		_parentNode.appendChild(_divNode);
+}
+
+/**
+ * _inputDivClick 当选中一个下拉列表选项时触发
+ * inputNodeId: 输入框的id
+ * divNodeId: 自动创建的div的id
+ * value: 待选值
+ */
+function _inputDivClick(inputNodeId, divNodeId, value) {
+	var inputNode = document.getElementById(inputNodeId),
+		divNode = document.getElementById(divNodeId);
+	inputNode.value = value;
+	var options = inputNode.parentNode.getElementsByTagName("option");
+	for (var loopIdx = 0; loopIdx < options.length; ++loopIdx) {
+		if (options[loopIdx].value.replace(/\ +/g, "").replace(/[\r\n]/g, "") == value) {
+			options[loopIdx].selected = true;
+		}
+	}
+	inputNode.parentNode.removeChild(divNode);
+}
+
+/**
+ * isInclude方法用于测试source是否包含有pattern这个字符串
+ * source: 待测试的字符串
+ * pattern:文本框输入的值
+ */
+function isIncluded(source, pattern) {
+	if (pattern == "")
+		return true;
+	var _reg = new RegExp(".*" + pattern + ".*");
+	return _reg.test(source);
+}
+
+//将要获取绝对位置的标签传进去，返回一个包含x和y属性的对象
+function getPosition(e) {
+	var t = e.offsetTop;
+	var l = e.offsetLeft;
+	while (e = e.offsetParent) {
+		t += e.offsetTop;
+		l += e.offsetLeft;
+	}
+	var point = eval("({x:" + l + ",y:" + t + "})");
+	return point;
+}
