c4679f486d66d36d927a3ae864b3885e0b5f1282
GiraKoo <girakoo@163.com>
2024-03-07 14:05:35 +0800

    feat: Change script server to izmj.net

--- jenkins_param_helper/jenkins-content-script.js
+++ jenkins_param_helper/jenkins-content-script.js
@@ -1,20 +1,21 @@
 // ==UserScript==
 // @name         	Jenkins编译辅助插件
-// @version      	1.4.0
+// @version      	1.4.1
 // @description  	进行编译的时候，提供额外的筛选按钮。如果功能不好使，请查看match路径是否正确
 // @author       	girakoo@163.com
-// @updateURL    	https://res.girakoo.com/jenkins_param_helper/jenkins-content-script.js
-// @downloadURL  	https://res.girakoo.com/jenkins_param_helper/jenkins-content-script.js
+// @updateURL    	https://res.izmj.net/jenkins_param_helper/jenkins-content-script.js
+// @downloadURL  	https://res.izmj.net/jenkins_param_helper/jenkins-content-script.js
 // @match      		*://*/*job/*/build*
 // @run-at       	document_end
 // ==/UserScript==
 
 // @history			1.4.0 移除特定IP的限制，通过匹配符进行访问
+// @history			1.4.1 切换服务器到izmj.net上
 
 var _headNode = document.getElementsByTagName('head')[0];
 var script = document.createElement("script");  //创建一个script标签
 script.type = "text/javascript";
-script.src = "https://res.girakoo.com/jenkins_param_helper/jenkins-function-script.js";
+script.src = "https://res.izmj.net/jenkins_param_helper/jenkins-function-script.js";
 _headNode.appendChild(script);
 
 //第一次加载的时候初始化样式文件
@@ -22,7 +23,6 @@ var setting_main_nodes = document.getElementsByClassName("setting-main");
 for (var loopIdx = 0; loopIdx < setting_main_nodes.length; ++loopIdx) {
 	var input_node = document.createElement("input");
 	var select_node = setting_main_nodes[loopIdx].getElementsByTagName("select")[0];
-
 
 	if (select_node != undefined) {
 		select_node.setAttribute("oninput", "showTip(this)");
